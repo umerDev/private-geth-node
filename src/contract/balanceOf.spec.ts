36fcaecaefad9026b43ebac4578f3885044e44fa
umerDev <umerraja@live.co.uk>
2025-05-06 11:57:41 +0100

    update balance of to use try catch

--- src/contract/balanceOf.spec.ts
+++ src/contract/balanceOf.spec.ts
@@ -27,4 +27,48 @@ describe('getBalance', () => {
     // assert
     expect(balance?.toString()).toEqual('1');
   });
+
+  it('should return null if address is invalid', async () => {
+    // arrange
+    const address = 'invalid_address';
+    const mockweb3 = {
+      eth: {
+        Contract: jest.fn().mockImplementation(() => ({
+          methods: {
+            balanceOf: jest.fn().mockImplementation(() => ({
+              call: jest.fn().mockResolvedValue(null),
+            })),
+          },
+        })),
+      },
+    } as unknown as Web3;
+
+    // act
+    const balance = await getBalance(mockweb3, tokenAddress, address);
+
+    // assert
+    expect(balance).toBeNull();
+  });
+
+  it('should return null if balanceOf call fails', async () => {
+    // arrange
+    const address = '0x9a0d29636f04bc14ad0ffe6a03703115251cdf21';
+    const mockweb3 = {
+      eth: {
+        Contract: jest.fn().mockImplementation(() => ({
+          methods: {
+            balanceOf: jest.fn().mockImplementation(() => ({
+              call: jest.fn().mockRejectedValue(new Error('Call failed')),
+            })),
+          },
+        })),
+      },
+    } as unknown as Web3;
+
+    // act
+    const balance = await getBalance(mockweb3, tokenAddress, address);
+
+    // assert
+    expect(balance).toBeNull();
+  });
 });
--- src/contract/balanceOf.ts
+++ src/contract/balanceOf.ts
@@ -1,19 +1,19 @@
+import { BN } from 'bn.js';
 import Web3 from 'web3';
-import {getERC20ABIOfInterest} from '../abi/ERC20';
-import {BN} from 'bn.js';
+import { getERC20ABIOfInterest } from '../abi/ERC20';
 
-export const getBalance = async (
-  web3Client: Web3,
-  tokenAddress: string,
-  walletAddress: string
-) => {
-  const balanceOfAbi = getERC20ABIOfInterest('balanceOf');
-  const contract = new web3Client.eth.Contract(balanceOfAbi, tokenAddress);
-  const result = await contract.methods.balanceOf(walletAddress).call();
+export const getBalance = async (web3Client: Web3, tokenAddress: string, walletAddress: string) => {
+  try {
+    const balanceOfAbi = getERC20ABIOfInterest('balanceOf');
+    const contract = new web3Client.eth.Contract(balanceOfAbi, tokenAddress);
+    const result = await contract.methods.balanceOf(walletAddress).call();
 
-  if (!result || Array.isArray(result)) return;
+    if (!result || Array.isArray(result)) return null;
 
-  const resultInEther = web3Client.utils.fromWei(result, 'ether');
-
-  return new BN(resultInEther);
+    const resultInEther = web3Client.utils.fromWei(result, 'ether');
+    return new BN(resultInEther);
+  } catch (error) {
+    console.error(`[getBalance]: unable to get balance for ${tokenAddress}`, { error });
+    return null;
+  }
 };
